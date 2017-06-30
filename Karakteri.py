447e65ecd99798395cf4ab3be1389d512512434f
Todor00 <majstorovict5@gmail.com>
2017-06-30 16:39:50 +0200

    Add files via upload

---
+++ Karakteri.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+niska = "Da li je moguće da sam i dalje niska?!"
+jedan_karakter = niska[37]
+isti_karakter = niska[-1]
+
+print(jedan_karakter)
+print(isti_karakter)
+
+nekoliko_karaktera = niska[0:10]
+print(nekoliko_karaktera)
+
+nekoliko_karaktera_s_kraja = niska[-15:-2]
