d773040425630c0a87937b87f66489e4aeda6dee
Todor00 <majstorovict5@gmail.com>
2017-07-01 17:01:19 +0200

    Add files via upload

---
+++ Niske.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+niska = "Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+niska = niska.lower()
+reci = niska.split(" ")
+print(reci)
+reci[4] = reci[4][:-1]
+print(reci[4])
+reci[5] = reci[5][:-1]
+print(reci[5])
+reci[12] = reci[12][:-1]
+print(reci[12])
+reci[13] = reci[13][1:-1]
+print(reci[13])
+reci[14] = reci[14][:-1]
+print(reci[14])
+reci[17] = reci[17][:-3]
+print(reci[17])
+reci[31] = reci[31][:-1]
+print(reci[31])
+reci[33] = reci[33][:-1]
+print(reci[33])
+reci[43] = reci[43][:-1]
+print(reci[43])
+reci[44] = reci[44][:-1]
+print(reci[44])
+reci[53] = reci[53][:-1]
+print(reci[53])
+reci[63] = reci[63][:-1]
+print(reci[63])
